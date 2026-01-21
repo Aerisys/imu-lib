a24d50b74e59d63bda746ed18731c3ea89833bf7
maxence <maxence.chachane@gmail.com>
2026-01-21 14:34:09 +0100

    add motorspeed

--- include/mpuDTO.h
+++ include/mpuDTO.h
@@ -3,11 +3,15 @@
 
 #include "mpu9250.h"
 
+#define NUM_MOTORS 4
+
 struct mpuDTO {
         MPU9250::Vector3 accel;
         MPU9250::Vector3 gyro;
         MPU9250::Vector3 mag;
         MPU9250::Orientation orientation;
+
+        float motorSpeeds[NUM_MOTORS];
     };
 
 #endif
